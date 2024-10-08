71212723a8d00b965f1940390235d0390c872966
im-ShreyaGupta <shreyaofficial1806@gmail.com>
2024-10-08 12:02:48 +0530

    add a prog

---
+++ Array.java
@@ -0,0 +1,14 @@
+class Array2
+{
+	public static void main(String[] args)
+	{
+		
+        int[]A2={65,89,32,64,67};
+	    int s=0;
+		for(int y=0;y<A2.length;y++)
+		{
+			s=s+A2[y];
+		}
+		System.out.print(s);
+    }
+}
